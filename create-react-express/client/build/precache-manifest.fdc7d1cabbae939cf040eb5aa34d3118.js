1c2e791060933e2847b077999395dde55f83ddb9
Non Dewman <Dnewman1934@yahoo.com>
2019-08-13 18:52:35 -0500

    init

---
+++ create-react-express/client/build/precache-manifest.fdc7d1cabbae939cf040eb5aa34d3118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a09c98631e59f38abae7",
+    "url": "/static/js/main.966c4498.chunk.js"
+  },
+  {
+    "revision": "61477f81220081e1df57",
+    "url": "/static/js/2.901827b4.chunk.js"
+  },
+  {
+    "revision": "a09c98631e59f38abae7",
+    "url": "/static/css/main.147ae2a6.chunk.css"
+  },
+  {
+    "revision": "6a2679e7cb5ec0d3d756e1ce05e055df",
+    "url": "/index.html"
+  }
+];
